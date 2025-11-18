685e765edd06d77866f2e5b55251efb3b63e1f8d
Арина Новгородова <arrrishanov@gmail.com>
2025-11-18 21:20:42 +0300

    add model Product

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Наименование продукта", help_text="Введите наименование продукта")
+    description = models.TextField(verbose_name="Описание продукта", help_text="Введите описание продукта")
+    image = models.ImageField(upload_to="catalog/image", blank=True, null=True, verbose_name="Изображение", help_text="Загрузите фото продукта")
+    category = models.ForeignObject(Category, on_delete=models.CASCADE)
+    cost = models.IntegerField(verbose_name="Цена")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата последнего изменения")
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["name", "category", "cost"]
+
+    def __str__(self):
+        return self.name
+
