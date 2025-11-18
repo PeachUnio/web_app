d29c41c6e5cf55fc311baadc110cb9d76bd4d8af
Арина Новгородова <arrrishanov@gmail.com>
2025-11-18 21:30:05 +0300

    add model Category

--- catalog/models.py
+++ catalog/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
+
 class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Наименование продукта", help_text="Введите наименование продукта")
-    description = models.TextField(verbose_name="Описание продукта", help_text="Введите описание продукта")
-    image = models.ImageField(upload_to="catalog/image", blank=True, null=True, verbose_name="Изображение", help_text="Загрузите фото продукта")
+    name = models.CharField(
+        max_length=100, verbose_name="Наименование продукта", help_text="Введите наименование продукта"
+    )
+    description = models.TextField(
+        blank=True, null=True, verbose_name="Описание продукта", help_text="Введите описание продукта"
+    )
+    image = models.ImageField(
+        upload_to="catalog/image",
+        blank=True,
+        null=True,
+        verbose_name="Изображение",
+        help_text="Загрузите фото продукта",
+    )
     category = models.ForeignObject(Category, on_delete=models.CASCADE)
     cost = models.IntegerField(verbose_name="Цена")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
@@ -17,3 +28,18 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100, verbose_name="Наименование категория", help_text="Введите наименование категории"
+    )
+    description = models.TextField(
+        blank=True, null=True, verbose_name="Описание категории", help_text="Введите описание категории"
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
