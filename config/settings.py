f672027a61d1f5d8c84b11d2aba34f5a09b152be
Арина Новгородова <arrrishanov@gmail.com>
2025-11-18 18:58:16 +0300

    add db settings

--- config/settings.py
+++ config/settings.py
@@ -52,23 +52,19 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "product_registry",
-        "USER": "postgres",
-        "PASSWORD": "KillJoy9907",
-        "DB_HOST": "localhost",
-        "DB_PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'product_registry',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
