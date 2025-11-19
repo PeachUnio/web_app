3e37d2e5923351365b4cd4e64e98add99ee7ffc5
Арина Новгородова <arrrishanov@gmail.com>
2025-11-19 18:58:46 +0300

    update models

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100, verbose_name="Наименование категория", help_text="Введите наименование категории"
+    )
+    description = models.TextField(
+        blank=True, null=True, verbose_name="Описание категории", help_text="Введите описание категории"
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(
         max_length=100, verbose_name="Наименование продукта", help_text="Введите наименование продукта"
@@ -16,7 +31,7 @@ class Product(models.Model):
         help_text="Загрузите фото продукта",
     )
     category = models.ForeignKey(
-        on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Категория", related_name="products"
+        Category, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Категория", related_name="products"
     )
     cost = models.IntegerField(verbose_name="Цена")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
@@ -26,22 +41,6 @@ class Product(models.Model):
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
         ordering = ["name", "category", "cost"]
-
-    def __str__(self):
-        return self.name
-
-
-class Category(models.Model):
-    name = models.CharField(
-        max_length=100, verbose_name="Наименование категория", help_text="Введите наименование категории"
-    )
-    description = models.TextField(
-        blank=True, null=True, verbose_name="Описание категории", help_text="Введите описание категории"
-    )
-
-    class Meta:
-        verbose_name = "Категория"
-        verbose_name_plural = "Категории"
 
     def __str__(self):
         return self.name
