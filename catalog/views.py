87563f4a48f7c9aab8720e4a4bbf6bfbfdfcc3ef
Арина Новгородова <arrrishanov@gmail.com>
2025-11-22 01:15:30 +0300

    update views

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from catalog.models import Product
 
 
@@ -9,3 +9,8 @@ def home(request):
 
 def contacts(request):
     return render(request, "contacts.html")
+
+def products_ditail(request, pk):
+    product = get_object_or_404(Product,pk=pk)
+    context = {"product": product}
+    return render(request, "products_ditail.html", context)
