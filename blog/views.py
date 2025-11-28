482cbdb49f6c41b3a0bd4df2249d8f61e9efef72
Арина Новгородова <arrrishanov@gmail.com>
2025-11-29 01:26:04 +0300

    add blog view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView
 
-# Create your views here.
+from blog.models import Publication
+
+
+class MainView(ListView):
+    model = Publication
+    template_name = "main_page.html"
+
+
+class PublicationCreateView(CreateView):
+    model = Publication
+    template_name = "template/blog_create.html"
+    fields = "name", "content", "image"
+    success_url = reverse_lazy("blog:")
