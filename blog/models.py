549f0a5a577c5f17b304bd4f84a993b80da37451
Арина Новгородова <arrrishanov@gmail.com>
2025-11-29 00:19:22 +0300

    create model for publications

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Publication(models.Model):
+    title = models.CharField(
+        max_length=100, verbose_name="Заголовок", help_text="Введите заголовок публикации"
+    )
+    content = models.TextField(verbose_name="Содержимое", help_text="Введите описание продукта")
+    image = models.ImageField(
+        upload_to="blog/image",
+        blank=True,
+        null=True,
+        verbose_name="Изображение",
+        help_text="Загрузите фото продукта",
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата публикации")
+    is_published = models.BooleanField(
+        default=False,
+        verbose_name="Опубликовано",
+        help_text="Отметьте для публикации"
+    )
+    views_count = models.PositiveIntegerField(
+        default=0,
+        verbose_name="Количество просмотров",
+        help_text="Количество просмотров статьи"
+    )
