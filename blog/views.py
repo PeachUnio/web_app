3d933137e8b924ec22b50b6531c6a4da74418594
Арина Новгородова <arrrishanov@gmail.com>
2025-11-30 20:16:01 +0300

    add views for publication

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DetailView
 
 from blog.models import Publication
 
@@ -10,8 +10,14 @@ class MainView(ListView):
     template_name = "main_page.html"
 
 
+class PublicationsDitail(DetailView):
+    model = Publication
+    template_name = "publication_ditail.html"
+    context_object_name = "publication"
+
+
 class PublicationCreateView(CreateView):
     model = Publication
-    template_name = "template/blog_create.html"
-    fields = "name", "content", "image"
-    success_url = reverse_lazy("blog:")
+    template_name = "blog_create.html"
+    fields = "title", "content", "image"
+    success_url = reverse_lazy("blog:main_page")
