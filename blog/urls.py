7a417967aa4ad740b252a904a57695ab9f42b11d
Арина Новгородова <arrrishanov@gmail.com>
2025-11-30 21:16:52 +0300

    add url for update

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from blog.apps import BlogConfig
-from blog.views import MainView, PublicationsDitail, PublicationCreateView
+from blog.views import MainView, PublicationsDitail, PublicationCreateView, PublicationUpdateView
 
 
 app_name = BlogConfig.name
@@ -9,4 +9,5 @@ urlpatterns = [
    path("", MainView.as_view(), name="main_page"),
    path("publication/<int:pk>/", PublicationsDitail.as_view(), name="publications_ditail"),
    path("create/", PublicationCreateView.as_view(), name="create"),
+   path("publication/<int:pk>/update/", PublicationUpdateView.as_view(), name="publications_update"),
 ]
