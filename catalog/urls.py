990f0b77861aefae1fff3382ce25a15b5b76ca91
Арина Новгородова <arrrishanov@gmail.com>
2025-12-09 20:25:09 +0300

    add url for create product

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from catalog.apps import CatalogConfig
-from catalog.views import HomeView, ContactsView, ProductsDitail
+from catalog.views import HomeView, ContactsView, ProductsDitail, ProductsCreateView
 
 app_name = CatalogConfig.name
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("contacts/", ContactsView.as_view(), name="contacts"),
     path("products/<int:pk>/", ProductsDitail.as_view(), name="products_ditail"),
+    path("product/create/", ProductsCreateView.as_view(), name="products_create"),
 ]
