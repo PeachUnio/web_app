a61eaf54f9104e5e9375ed0fb1526f50100e0f12
Арина Новгородова <arrrishanov@gmail.com>
2025-12-09 20:23:25 +0300

    add CreateView for product

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, TemplateView, DetailView
+from django.views.generic import ListView, TemplateView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
 
 from catalog.models import Product
+from catalog.forms import ProductForm
 
 
 class HomeView(ListView):
@@ -17,3 +19,27 @@ class ProductsDitail(DetailView):
     model = Product
     template_name = "catalog/products_ditail.html"
     context_object_name = "product"
+
+
+class ProductsCreateView(CreateView):
+    model = Product
+    template_name = "catalog/product_form.html"
+    form_class = ProductForm
+    success_url = reverse_lazy("catalog:home")
+
+
+# class ProductsUpdateView(UpdateView):
+#     model = Product
+#     template_name = "product_form.html"
+#     form = ProductForm
+#     success_url = reverse_lazy("blog:main_page")
+#
+#     def get_success_url(self):
+#         return reverse("blog:publications_ditail", args=[self.kwargs.get("pk")])
+#
+#
+# class ProductsDeleteView(DeleteView):
+#     model = Product
+#     template_name = "publication_confirm_del.html"
+#     success_url = reverse_lazy("blog:main_page")
+#     context_object_name = "publication"
