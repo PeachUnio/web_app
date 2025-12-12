a06d5d0fd4f2e1345a9eb239ed651e1bcf0ff0e1
Арина Новгородова <arrrishanov@gmail.com>
2025-12-12 18:16:48 +0300

    style form

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,9 +1,19 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, BooleanField
 from catalog.models import Product
 
 
-class ProductForm(ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs["class"] = "form-check-input"
+            else:
+                field.widget.attrs["class"] = "form-control"
+
+
+
+class ProductForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Product
         fields = ("name", "description", "category", "image", "cost")
-
