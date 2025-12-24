4cf8613b435cc77b0dafa341afb6ba79275d59f9
Арина Новгородова <arrrishanov@gmail.com>
2025-12-24 18:58:34 +0300

    fix email fild

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class User(AbstractUser):
     username = None
-    email = (models.EmailField(unique=True, verbose_name="Почта"),)
+    email = models.EmailField(unique=True, verbose_name="Почта")
     phone = models.CharField(max_length=35, verbose_name="Номер телефона", blank=True, null=True)
     avatar = models.ImageField(upload_to="user/avatars/", blank=True, null=True, verbose_name="Аватарка")
     country = models.CharField(max_length=56)
