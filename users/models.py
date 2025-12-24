a521817d1dae5674d31626c82feaf32ebf6e362e
Арина Новгородова <arrrishanov@gmail.com>
2025-12-24 20:38:32 +0300

    create custom manager

--- users/models.py
+++ users/models.py
@@ -1,16 +1,46 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+
+class UserManager(BaseUserManager):
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Почта не введена')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser должен быть сотрудником.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser должен быть разрешен доступ.')
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
     username = None
-    email = models.EmailField(unique=True, verbose_name="Почта")
+    email = models.EmailField(unique=True, verbose_name="Email")
     phone = models.CharField(max_length=35, verbose_name="Номер телефона", blank=True, null=True)
     avatar = models.ImageField(upload_to="user/avatars/", blank=True, null=True, verbose_name="Аватарка")
     country = models.CharField(max_length=56)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     class Meta:
         verbose_name = "Пользователь"
