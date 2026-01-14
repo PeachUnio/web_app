71d45e8b78279268a489c9e699ba4ac1bb5bb338
Арина Новгородова <arrrishanov@gmail.com>
2026-01-14 21:20:50 +0300

    add new permission

--- catalog/models.py
+++ catalog/models.py
@@ -42,6 +42,7 @@ class Product(models.Model):
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
         ordering = ["name", "category", "cost"]
+        permissions = [("can_unpublish_product", "Can unpublish product"),]
 
     def __str__(self):
         return self.name
