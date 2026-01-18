56c63ee0a9aace6eeae69bbe482b18daa452c2de
Арина Новгородова <arrrishanov@gmail.com>
2026-01-18 18:03:30 +0300

    update function for cache with filter

--- catalog/services.py
+++ catalog/services.py
@@ -4,12 +4,13 @@ from django.core.cache import cache
 
 
 def get_products_from_cache():
+    """Получает данные о продукте из кеша, если он пуст, делает запрос бд"""
     if not CACHE_ENABLED:
-        return Product.objects.all()
+        return Product.objects.filter(publish_product=True).order_by('name', 'category', 'cost')
     key = "products_list"
     products = cache.get(key)
     if products is not None:
         return products
-    products = Product.objects.all()
+    products = Product.objects.filter(publish_product=True).order_by('name', 'category', 'cost')
     cache.set(key, products)
     return products
