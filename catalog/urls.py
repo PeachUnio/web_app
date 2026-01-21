e885b4fdb41353f7af9e2fb971d5ce15f33f7144
Арина Новгородова <arrrishanov@gmail.com>
2026-01-21 19:38:25 +0300

    add uri for category list

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from django.views.decorators.cache import cache_page
 
 from catalog.apps import CatalogConfig
-from catalog.views import HomeView, ContactsView, ProductsDitail, ProductsCreateView, ProductUpdateView, ProductDeleteView, CategoryProductsView
+from catalog.views import HomeView, ContactsView, ProductsDitail, ProductsCreateView, ProductUpdateView, ProductDeleteView, CategoryProductsView, CategoryListView
 
 app_name = CatalogConfig.name
 
@@ -13,6 +13,6 @@ urlpatterns = [
     path("product/create/", ProductsCreateView.as_view(), name="products_create"),
     path("product/<int:pk>/update/", ProductUpdateView.as_view(), name="products_update"),
     path("product/<int:pk>/delete/", ProductDeleteView.as_view(), name="products_delete"),
-    # path('categories/', CategoryListView.as_view(), name='category_list'),
-    path('category/<int:category_id>/products/', CategoryProductsView.as_view(), name='category_products'),
+    path("categories/", CategoryListView.as_view(), name='category_list'),
+    path("category/<int:category_id>/products/", CategoryProductsView.as_view(), name="category_products"),
 ]
